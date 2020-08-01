88c8944ce242960f56d12c6c81c8d95526ca19af
Junha Park <jpark3@scu.edu>
2020-07-31 18:09:56 -0700

    Spekkio comes to GitHub with a CLI

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spekkio CLI
+
+View the core Spekkio repository [here](https://github.com/spekkio-bot/spekkio)!
---
+++ src/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Spekkio, the Master of War, comes to GitHub with a CLI!")
+}
